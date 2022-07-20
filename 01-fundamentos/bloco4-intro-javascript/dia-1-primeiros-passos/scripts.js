e39923e03571ec2d64903861f2bb964d20ed94f0
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-07-20 16:05:03 -0300

    exercício proposto parte V

--- 01-fundamentos/bloco4-intro-javascript/dia-1-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco4-intro-javascript/dia-1-primeiros-passos/scripts.js
@@ -1,18 +1,8 @@
-const currentHour = 16;
-let message= "";
-if (currentHour >= 22){
-    message = "Não deveríamos comer nada, está na hora de dormir";
+let weekDay = 'sabadão';
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || 
+weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log("Oba, mais um dia de aprendizado na trybe >:D");
 }
-else if (currentHour >=18 && currentHour< 22){
-    message = "Rango da noite, vamos jantar :D";
+else {
+    console.log("FINALMENTE, descanso merecido UwU");
 }
-else if (currentHour >=14 && currentHour < 18){
-    message = "vamos fazer um bolo para o café da tarde?";
-}
-else if (currentHour >= 11 && currentHour < 14) {
-    message = "Hora do Almoço!!!";
-}
-else if (currentHour >= 4 && currentHour < 11){
-    message = "Hmmmm, cheiro de café recém passado";
-}
-console.log(message);

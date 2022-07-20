dd8bffb17d78de941d4555a591dd4c674b45d055
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-07-20 15:45:58 -0300

    exercício proposto parte III

--- 01-fundamentos/bloco4-intro-javascript/dia-1-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco4-intro-javascript/dia-1-primeiros-passos/scripts.js
@@ -1,8 +1,5 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
-console.log(typeof patientEmail, typeof patientId, typeof patientInfo, typeof isEnrolled);
+const base = 5;
+let height = 8;
+const area = base*height;
+const perimetro = base + base + height +height;
+console.log(area, perimetro);

c16c6ab7068e52e51b942209698a0837b298963e
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-07-21 15:41:21 -0300

    Criado scripts.js para realização dos exercícios de fixação

---
+++ 01-fundamentos/bloco4-intro-javascript/dia-2-array-e-loop-for/scripts.js
@@ -0,0 +1,45 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.shift();  // remove a última tarefa
+// console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião' ]
+
+//EXERCICIO I
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//EXERCICIO II
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+//EXERCICIO III
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato');
+// console.log(menu);
+
+//EXERCICIO IV
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for(let i=0; i< groceryList.length; i+=1){
+//     console.log(groceryList[i]);
+// }
+
+//EXERCICIO V
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for (let nomes of names){
+//     console.log(nomes);
+// }
+
+
+function dice () { 
+    return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+    d2 = dice();
+}

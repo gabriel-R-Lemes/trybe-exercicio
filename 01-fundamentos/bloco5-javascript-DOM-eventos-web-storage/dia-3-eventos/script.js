eb594c8fc40b96eede06ff638c19c5fd2c633094
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-08-01 15:43:00 -0300

    Realização do Exercicio IX

--- 01-fundamentos/bloco5-javascript-DOM-eventos-web-storage/dia-3-eventos/script.js
+++ 01-fundamentos/bloco5-javascript-DOM-eventos-web-storage/dia-3-eventos/script.js
@@ -108,3 +108,44 @@ function subtitleTasks(cor){
 
 }
 subtitleTasks('red');
+
+// function chooseTask (){
+//   let taskDiv = document.querySelector('.task');
+//   taskDiv.addEventListener('click', function(){
+//     if (taskDiv.className !== 'task-selected'){
+//       taskDiv.className = 'task-selected';
+//     } else {
+//       taskDiv.className = 'task';
+//     }
+//   })
+// }
+function setTaskClass() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+  myTasks.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+}
+setTaskClass();
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color; 
+    } else if (eventTargetColor === taskColor) {
+      event.target.style.color = 'rgb(119,119,119)';  
+    }
+  });
+}
+setDayColor();

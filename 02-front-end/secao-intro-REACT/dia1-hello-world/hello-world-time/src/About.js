1fd9deaf45e118d904e2e6b2acbaa49d86fc1472
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-09-20 14:57:36 -0300

    Seção do dia 1 de REACT

---
+++ 02-front-end/secao-intro-REACT/dia1-hello-world/hello-world-time/src/About.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+class About extends React.Component{
+  render() {
+    return (
+      <>
+        <h1>Gabriel Lemes</h1>
+        <p>Hello World!! Estudante da Trybe, futuro dev pleno!</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>GIT</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>DOM</li>
+          <li>localStorage</li>
+          <li>Requisições API</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default About
